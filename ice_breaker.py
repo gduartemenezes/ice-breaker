9d3f94cb7dae0b347d1437c6d9808c973636dc96
Gustavo Duarte <gduartemenezes@gmail.com>
2024-10-08 09:17:06 -0300

    feat: import agent and develop logic to generate data based on linkedin

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,12 +1,16 @@
 from dotenv import load_dotenv
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
 load_dotenv()
 
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
-if __name__ == '__main__':
+def ice_break_with(name: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_url)
     summary_template =  """
         given the linkedin information {information} from a person I want you to create:
         1. a short summary
@@ -16,11 +20,13 @@ if __name__ == '__main__':
     summary_prompt_template = PromptTemplate(input_variables="information", template=summary_template)
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
     chain = summary_prompt_template | llm
     
-    linkedin_data = scrape_linkedin_profile("Gustavo Duarte")
+
 
     res = chain.invoke(input={"information": linkedin_data})
-
     print(res)
+
+
+if __name__ == '__main__':
+   ice_break_with("Gustavo Duarte Menezes Goiânia")
