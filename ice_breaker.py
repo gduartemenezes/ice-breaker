ef4231f09def5be09781795bb8ec03848d256ba5
Gustavo Duarte <gduartemenezes@gmail.com>
2024-10-08 09:46:18 -0300

    feat: implement output_parser into chain

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-
+from output_parsers import summary_parser
 load_dotenv()
 
 from langchain_core.prompts import PromptTemplate
@@ -10,17 +10,23 @@ from langchain_openai import ChatOpenAI
 
 def ice_break_with(name: str) -> str:
     linkedin_url = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_url)
+    linkedin_data = scrape_linkedin_profile(linkedin_url, mock=True)
     summary_template =  """
         given the linkedin information {information} from a person I want you to create:
         1. a short summary
         2. two interesting facts about them
+
+        \n{format_instructions}
     """
 
-    summary_prompt_template = PromptTemplate(input_variables="information", template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables="information",
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
+    )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
     
 
 
